a82f742425e6d1da83d64d0cffd1c5883afe7b86
Harsh Koli <harshkoli1255@gmail.com>
2026-08-18 03:14:06 +0530

    successfully fixed eye icon on password field

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import { Eye, EyeOff } from 'lucide-react'
 import { useState } from 'react'
-const Input = ({icon : Icon, type, value, ...props}) => {
-
+const Input = ({icon : Icon, value, type, ...props}) => {
+  let showType = type;
   const [isEyeClicked, setisEyeClicked] = useState(false);
-  const handleEyeClick = (e) => {
-    e.preventDefault();
+  const handleEyeClick = () => {
     setisEyeClicked(prev => !prev)
   }
   return (
@@ -14,19 +13,19 @@ const Input = ({icon : Icon, type, value, ...props}) => {
             <Icon className="w-5 h-5 text-blue-500"/>
         </div>
 
-        <input {...props} type={isEyeClicked ? "password" : "text"} className='w-full pl-10 pr-3 py-2 bg-gray-800 bg-opacity-50 rounded-lg border border-gray-700 focus:border-blue-500 focus:ring-2 focus:ring-blue-500 text-white placeholder-gray-400 transition duration-200'/>
+        <input {...props} type={isEyeClicked ? type=="password" : showType}className='w-full pl-10 pr-3 py-2 bg-gray-800 bg-opacity-50 rounded-lg border border-gray-700 focus:border-blue-500 focus:ring-2 focus:ring-blue-500 text-white placeholder-gray-400 transition duration-200'/>
 
         {value.length > 0 && type=="password" && (
           <div className="absolute inset-y-0 right-0 flex items-center pr-2">
             <button
               type="button"
-              onClick={handleEyeClick}
+              onClick={() => {handleEyeClick()}}
               className="p-2 rounded-md text-blue-400 
                         hover:text-blue-300 
                         transition-all duration-200
                         cursor-pointer"
             >
-              {isEyeClicked ? (
+              {!isEyeClicked ? (
                 <Eye className="w-5 h-5" />
               ) : (
                 <EyeOff className="w-5 h-5" />
